8d65cbbc41fe772a9b2c1ccc6d84fece540dc4a2
Solucity <luchoferreyra211442@gmail.com>
2025-10-02 21:44:57 -0300

    style: aplicar formato prettier en configs

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  "*.{js,jsx}": ["eslint --fix", "prettier --write"],
-  "*.{ts,tsx}": ["eslint --fix", "prettier --write", "tsc --noEmit --pretty false"],
-  "*.{json,md,yml,yaml}": ["prettier --write"]
-};
+  '*.{js,jsx}': ['eslint --fix', 'prettier --write'],
+  '*.{ts,tsx}': ['eslint --fix', 'prettier --write', 'tsc --noEmit --pretty false'],
+  '*.{json,md,yml,yaml}': ['prettier --write'],
+}

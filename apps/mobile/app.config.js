2b8ac729417a01fc7aa0e5b853891075cfaa1786
Solucity <luchoferreyra211442@gmail.com>
2026-02-17 11:09:11 -0300

    fix(repo): actualiza versión para generar AAB

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -3,7 +3,7 @@
 module.exports = {
   name: 'Solucity',
   slug: 'solucity',
-  version: '1.1.4',
+  version: '1.1.5',
   orientation: 'portrait',
   scheme: 'solucity',
 
@@ -31,7 +31,7 @@ module.exports = {
 
   android: {
     package: 'com.solucity.app',
-    versionCode: 30,
+    versionCode: 31,
     blockedPermissions: ['android.permission.RECORD_AUDIO'],
     googleServicesFile: './google-services.json',
     adaptiveIcon: {

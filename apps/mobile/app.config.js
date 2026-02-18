a1421d63c5691033b195cdcb94fce2769af1dd69
Solucity <luchoferreyra211442@gmail.com>
2026-02-18 09:05:44 -0300

    chore(mobile): bump app version for internal testing

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -3,7 +3,7 @@
 module.exports = {
   name: 'Solucity',
   slug: 'solucity',
-  version: '1.1.7',
+  version: '1.1.8',
   orientation: 'portrait',
   scheme: 'solucity',
 
@@ -31,7 +31,7 @@ module.exports = {
 
   android: {
     package: 'com.solucity.app',
-    versionCode: 33,
+    versionCode: 34,
     blockedPermissions: ['android.permission.RECORD_AUDIO'],
     googleServicesFile: './google-services.json',
     adaptiveIcon: {

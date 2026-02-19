d2770e21798cf62afdcb3338b9f89a62dc9e6416
Solucity <luchoferreyra211442@gmail.com>
2026-02-19 17:02:43 -0300

    fix(repo): bump android versionCode to 35 for production release

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -3,7 +3,7 @@
 module.exports = {
   name: 'Solucity',
   slug: 'solucity',
-  version: '1.1.8',
+  version: '1.1.9',
   orientation: 'portrait',
   scheme: 'solucity',
 
@@ -31,7 +31,7 @@ module.exports = {
 
   android: {
     package: 'com.solucity.app',
-    versionCode: 34,
+    versionCode: 35,
     blockedPermissions: ['android.permission.RECORD_AUDIO'],
     googleServicesFile: './google-services.json',
     adaptiveIcon: {

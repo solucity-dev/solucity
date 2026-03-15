4780c2895f3eca39977e024d14095fcafab5baba
Solucity <luchoferreyra211442@gmail.com>
2026-03-15 18:57:58 -0300

    fix(repo): bump mobile app version for new internal build

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -3,7 +3,7 @@
 module.exports = {
   name: 'Solucity',
   slug: 'solucity',
-  version: '1.1.14',
+  version: '1.1.15',
   orientation: 'portrait',
   scheme: 'solucity',
 
@@ -31,7 +31,7 @@ module.exports = {
 
   android: {
     package: 'com.solucity.app',
-    versionCode: 40,
+    versionCode: 41,
     blockedPermissions: ['android.permission.RECORD_AUDIO'],
     googleServicesFile: './google-services.json',
     adaptiveIcon: {
